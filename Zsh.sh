257edb36b0389fd4eb6c3c9026b6632308ba071a
Nicolas <nicolasperaltait@gmail.com>
2024-05-13 00:56:28 -0300

    Update Zsh.sh
    actaulizacion

--- Zsh.sh
+++ Zsh.sh
@@ -1,46 +1,52 @@
 #!/bin/bash
 
-echo "----------- zsh -----------"
+# ========================================================================================================================= #
+echo "----------- Instalación de ZSH -----------"
 
-
-echo "ZSH INSTALL"
-
+echo "Instalando ZSH y curl"
 sudo apt install zsh curl -y
 
-echo "----------- oh my zsh -----------"
 
-
+# ========================================================================================================================= #
+echo "----------- Instalación de oh my zsh -----------"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
-echo "----------- fuentes zsh -----------"
+# ========================================================================================================================= #
+echo "----------- Instalación de fuentes para ZSH -----------"
+
 
 # Clonar el repositorio zsh-font desde GitHub
-cd github/
+cd github/ && git clone https://github.com/Nicolasperaltait/ZSH-Fonts.git && cd ZSH-Fonts/
 
-git clone https://github.com/Nicolasperaltait/ZSH-Fonts.git
 
-cd ZSH-Fonts/
+mkdir -p ~/.local/share/fonts 
 
-# Instalar las fuentes zsh
-sudo apt install MesloLGS NF Bold Italic.ttf MesloLGS NF Bold Italic.ttf MesloLGS NF Bold.ttf MesloLGS NF Italic.ttf
+ls ttf/*  && fc-cache -f -v  # lista y actualiza cache de fuentes 
 
-# Regresar a la carpeta original
-cd ..
+cp -r github/debian12/ZSH-Fonsts/* ~/.local/share/fonts/ # esto copia todo el contenido en la carpeta fonts 
 
-echo "----------- fuentes zsh instaladas -----------"
+fc-cache -f -v  && cd .. # Regresar a la carpeta original
 
-echo "----------- instalacion oh my zsh con curl -----------"
 
-#clonado del repositorio
+echo "----------- Fuentes de ZSH instaladas -----------"
+
+
+# ========================================================================================================================= #
+echo "----------- Instalación de oh my zsh con curl -----------"
+
+# Clonado del repositorio
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-#Vitamina para la terminal 
-
+# Plugins para la terminal
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo  "----------- completa el archivo y mandale mecha -----------"
+
+# ========================================================================================================================= #
+
+
+echo "----------- Completa el archivo, ¡Mandale mecha! -----------"
 
 cd / && cd home/nicolas
 
