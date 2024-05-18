8fb392ad8e1ee47ec4734976c5237ca50b0d47b3
Nicolas <nicolasperaltait@gmail.com>
2024-05-18 20:26:32 -0300

    fix
    Se acomodo el script para mejor lectura

--- basicos.sh
+++ basicos.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-echo "Debian basic packages"
+
+# Actualizacion 
+
+echo "=================== Actualizacion =================="
 sudo apt update && sudo apt upgrade -y &&
-sudo apt install nala -y  && 
-sudo nala install wget git curl neofetch ufw clamav clamav-daemon font-manager zsh htop preload -y
 
-echo "Packages installed" 
+echo "================== Instalacion Basicos =================="
+sudo apt install nala -y  && sudo nala install wget git curl ufw clamav clamav-daemon font-manager zsh htop preload kitty -y
 
-echo "Basic configuration"
+# Configuraciones basicas
 
-echo "ufw configuration"
+echo "================== Firewall configuration =================="
 sudo ufw enable && sudo ufw default deny incoming && sudo ufw default allow outgoing && 
 
-echo "clamav configuration"
+echo "================== Anti-Virus configuration =================="
 
-# Detener el servicio de actualización de ClamAV
-sudo systemctl stop clamav-freshclam && 
-# Actualizar la base de datos de firmas de virus de ClamAV
-sudo freshclam && 
-# Iniciar de nuevo el servicio de actualización de ClamAV
-sudo systemctl start clamav-freshclam
-# Habilitar y iniciar el servicio de ClamAV
-sudo systemctl enable clamav-daemon && sudo systemctl start clamav-daemon
+sudo systemctl stop clamav-freshclam &&  # Detener el servicio de actualización de ClamAV
+sudo freshclam &&                        # Actualizar la base de datos de firmas de virus de ClamAV
+sudo systemctl start clamav-freshclam && # Iniciar de nuevo el servicio de actualización de ClamAV
+sudo systemctl start clamav-daemon &&    # Habilitar y iniciar el servicio de ClamAV
 
 echo "Basic configuration done"
