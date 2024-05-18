db1b3c7ef2bed8673500d3784496c674b08eb974
Nicolas <nicolasperaltait@gmail.com>
2024-05-18 19:33:42 -0300

    Update Cleaner.sh
    fix lotes

--- Cleaner.sh
+++ Cleaner.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Este script es para limpiar la totalidad del bloatware en debian 12 KDE 
-
-# Otorgar permisos de ejecución al script
-chmod +x $0
-
 #por si te olvidas el nombre kdeconnect-sms 
 
-# Purga por lotes 
-sudo apt -f purge 'akonadi*' 'libreoffice*' 'kdeconnect*'-y && 
+echo "Purga por lotes "
 
-# Purga individual
+sudo apt -f purge 'akonadi*' &&
+sudo apt -f purge 'libreoffice*' &&
+sudo apt -f purge 'kdeconnect*'-y &&
+
+echo "Purga individual"
+
 sudo apt remove kate gwenview gimp imagemagick uxterm vim sweeper spectacle sieveeditor kwallet konsole konqueror -y &&
 
-#Limpieza de paquetes obsoletos
+echo "Limpieza de paquetes obsoletos"
+
 sudo apt autoremove && sudo apt autoclean && sudo apt clean -y &&
 
-#Actualización de paquetes
+echo "Actualización de paquetes"
 
 sudo apt update && sudo apt upgrade -y
 
