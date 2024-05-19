44904e1e38d77b1d2a0a55c5126aeb3aee13d53f
Nicolas <nicolasperaltait@gmail.com>
2024-05-19 17:37:36 -0300

    fix
    error de estrunctura

--- 00-Script.sh
+++ 00-Script.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# Este script es el Principal y será el encargado de llamar a los demás scripts
 
-# Este script es el Principal y sera el encargado de llamar a los demas scripts
+# Cambiamos al directorio home de Nicolas
+cd /home/nicolas
 
-cd / && cd /home/nicolas
-
-sudo mkdir github
-
-cd github
-
+# Clonamos el repositorio directamente en el directorio home de Nicolas
 git clone https://github.com/Nicolasperaltait/Debian12.git
 
-cd Debian12
+# Creamos el directorio github si no existe
+mkdir -p github
 
+# Movemos el contenido clonado al directorio github
+mv Debian12 github/
+
+# Cambiamos al directorio donde se movió el repositorio
+cd github/Debian12
+
+# Cambiamos los permisos de los scripts
 sudo chmod 775 basicos.sh && sudo chmod 775 Cleaner.sh 
 
-sudo apt ./basicos.sh && sudo apt ./Cleaner-KDE.sh
+# Ejecutamos los scripts
+sudo ./basicos.sh && sudo ./Cleaner-KDE.sh
