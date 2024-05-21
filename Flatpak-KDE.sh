3afaeb37d8228a60d16c912c6bfd6968798b2881
Nicolas <138190212+Nicolasperaltait@users.noreply.github.com>
2024-05-21 14:56:00 -0300

    Flatpak-KDE.sh
    Creacion Base de .sh Flatpak

---
+++ Flatpak-KDE.sh
@@ -0,0 +1,8 @@
+# instalacion Flatpak KDE - Para genome es necesario otro Pluging 
+
+
+sudo apt install flatpak -y 
+
+sudo apt install plasma-discover-backend-flatpak
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
