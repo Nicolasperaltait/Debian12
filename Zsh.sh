df5de72ca5f5f84483d96a111443808833817842
Nicolas <nicolasperaltait@gmail.com>
2024-05-25 17:57:19 -0300

    fix

--- Zsh.sh
+++ Zsh.sh
@@ -5,17 +5,11 @@ sudo nala install curl zsh  -y
 
 mkdir -p usr/share/fonts 
 
-# instalar fuentes 
-cd / && cd home/nicolas/Debian12/Fonts-ttf/ 
-
-
-&& sudo cp *.ttf /usr/share/fonts 
-
-
-&& fc-cache -f -v
+# instalar fuentes // las movemos delsde el repo al sistema // actualizamos el cache 
+cd / && cd home/nicolas/Debian12/Fonts-ttf/ && sudo cp *.ttf /usr/share/fonts && fc-cache -f -v
 
 # Se descarga oh my zsh con curl
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y &&
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # clonamos el repo de powelevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -25,12 +19,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # Nos ubicamos en el directorio correcto y copiamos el .zsh indicado y lo copiamos donde corresponde
 cd / && cd home/nicolas/Debian12 && sudo mv .zshrc /home/nicolas/
-
-
-
-
-
-
-
-
 
