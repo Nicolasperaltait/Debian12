1000a95a7fb35ab6f26883ef8682084bbc876513
Nicolas <nicolasperaltait@gmail.com>
2024-05-25 16:51:29 -0300

    fiz

--- 00-Script.sh
+++ 00-Script.sh
@@ -6,17 +6,6 @@
 
 #============================================================================================================================#
 
-# Clonamos el repositorio
-git clone https://github.com/Nicolasperaltait/Debian12.git
-
-# Creamos el directorio github si no existe
-mkdir -p github
-
-# Movemos el contenido clonado al directorio github
-mv Debian12 github/
-
-# Cambiamos al directorio donde se movió el repositorio
-cd github/Debian12
 
 # Cambiamos los permisos de los scripts a ejecución
 sudo chmod 755 *
