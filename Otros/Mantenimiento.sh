05b22920fdae059e8d1369988a7dc3415d83f978
Nicolas <nicolasperaltait@gmail.com>
2024-05-26 10:11:07 -0300

    Primer Test Branch Test

--- Otros/Mantenimiento.sh
+++ Otros/Mantenimiento.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-# Este script realiza una actualización y limpieza del sistema en Debian o distribuciones basadas en Debian.
 
-echo "---------- ACTUALIZACION ----------"
-sudo apt update && sudo apt upgrade -y 
 
-echo "---------- LIMPIEZA ----------"
-# Elimina los paquetes ya no necesarios en el sistema.
-sudo apt autoclean 
-# Elimina paquetes instalados automáticamente y que ya no son necesarios.
-sudo apt autoremove
-# Elimina también los archivos de configuración residuales.
-sudo apt autoremove --purge
+# Script de Limpieza y base debian. 
+# Es necesario tener nala instalado. sino reemplazar por apt 
 
-echo "---------- ACTUALIZACION y LIMPIEZA FINALIZADA ----------"
+echo "================== ACTUALIZACION =================="
+sudo nala update && sudo nala upgrade -y 
+
+echo "================== LIMPIEZA =================="
+sudo nala autoclean             # Elimina los paquetes ya no necesarios en el sistema.
+sudo nala autoremove            # Elimina paquetes instalados automáticamente y que ya no son necesarios.
+sudo nala autoremove --purge    # Elimina también los archivos de configuración residuales.
+
+echo "================== Anti-Virus  =================="
+sudo nala install clamav -y && sudo nala install clamav-daemon -y 
+
+sudo systemctl stop clamav-freshclam   # Detener el servicio de actualización de ClamAV
+sudo freshclam                         # Actualizar la base de datos de firmas de virus de ClamAV
+sudo systemctl start clamav-freshclam  # Iniciar de nuevo el servicio de actualización de ClamAV
+sudo systemctl start clamav-daemon     # Habilitar y iniciar el servicio de ClamAV
+
+echo "================== ACTUALIZACION y LIMPIEZA FINALIZADA =================="
 exit
-# Finaliza el script.
+
