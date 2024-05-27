fb095faa34171d6aa14231c030654765a4ff4b2a
Nicolas <nicolasperaltait@gmail.com>
2024-05-27 20:13:44 -0300

    Dotfiles

--- Zsh.sh
+++ Zsh.sh
@@ -15,8 +15,10 @@ sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/maste
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Descargamos Plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
 
 # Nos ubicamos en el directorio correcto y copiamos el .zsh indicado y lo copiamos donde corresponde
-cd / && cd home/nicolas/Debian12 && sudo mv .zshrc /home/nicolas/
+cd / && cd home/nicolas/Debian12/Dotfiles && sudo cp .zshrc /home/nicolas/
 
