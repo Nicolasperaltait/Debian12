a119ff5fe6a33585c544c9b036d76e9c98a46ecc
Nicolas <nicolasperaltait@gmail.com>
2024-05-29 22:21:33 -0300

    .

--- OhMyZsh.sh
+++ OhMyZsh.sh
@@ -13,3 +13,14 @@ cd / && cd home/nicolas/Debian12/Fonts-ttf/ && sudo cp *.ttf /usr/share/fonts &&
 echo "Iniciando la descarga de ohmyzsh - Esto puede tardar varios minutos..."
 
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y 
+
+# clonamos el repo de powelevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Descargamos Plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
+
+# Nos ubicamos en el directorio correcto y copiamos el .zsh indicado y lo copiamos donde corresponde
+cd / && cd home/nicolas/Debian12/Dotfiles && sudo cp .zshrc /home/nicolas/
