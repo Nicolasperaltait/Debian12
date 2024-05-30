e85eadafc625b5a801da7567fbd16ba7db9c04f3
Nicolas <nicolasperaltait@gmail.com>
2024-05-29 22:40:04 -0300

    Test zsh autocomprobacion

--- 00-Script.sh
+++ 00-Script.sh
@@ -12,4 +12,4 @@
 sudo chmod 755 *
 
 # Ejecutamos los scripts
-sudo ./Start.sh && sudo ./Cleaner-KDE.sh && sudo ./EdgeRepo.sh && sudo ./Flatpak-KDE.sh && sudo ./Zsh.sh
+sudo ./Start.sh && sudo ./Cleaner-KDE.sh && sudo ./EdgeRepo.sh && sudo ./Flatpak-KDE.sh && sudo ./OhMyZsh.sh
--- OhMyZsh.sh
+++ OhMyZsh.sh
@@ -6,21 +6,22 @@ sudo nala install curl zsh  -y
 mkdir -p usr/share/fonts 
 
 # instalar fuentes // las movemos delsde el repo al sistema // actualizamos el cache 
-cd / && cd home/nicolas/Debian12/Fonts-ttf/ && sudo cp *.ttf /usr/share/fonts && fc-cache -f -v
+cd / && cd home/nicolas/Debian12/Fonts-ttf/ && sudo cp *.ttf /usr/share/fonts && fc-cache -f -v &&
 
+echo "=================== Iniciando la descarga de ohmyzsh - Esto puede tardar varios minutos ===================" && 
 
 # Se descarga oh my zsh con curl
-echo "Iniciando la descarga de ohmyzsh - Esto puede tardar varios minutos..."
 
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y 
+echo "y" | sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && 
 
 # clonamos el repo de powelevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k 
 
 # Descargamos Plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion &&
 
 # Nos ubicamos en el directorio correcto y copiamos el .zsh indicado y lo copiamos donde corresponde
 cd / && cd home/nicolas/Debian12/Dotfiles && sudo cp .zshrc /home/nicolas/
