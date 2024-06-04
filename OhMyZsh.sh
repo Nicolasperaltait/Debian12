3ee56f7acaee6ba44026c6bfcb202030b6bff7b9
Nicolas <nicolasperaltait@gmail.com>
2024-06-04 10:40:47 -0300

    fix

--- OhMyZsh.sh
+++ OhMyZsh.sh
@@ -5,17 +5,14 @@
 
 #======================================================================================================================================#
 
-sudo nala install curl zsh -y                                                                                                             # Instalamos curl y zsh 
+sudo nala install curl zsh ufw -y                                                                                                      # Instalamos curl y zsh 
 
-mkdir -p /usr/share/fonts                                                                                                                 # Creamos el directorio donde se copiarán las fuentes
-
-cd /home/nicolas/debian12/Fonts-ttf/ && sudo cp *.ttf /usr/share/fonts && fc-cache -f -v                                                  # Instalar fuentes // las movemos del repo al sistema // actualizamos el cache 
+mkdir -p /usr/share/fonts && cd / && cd /home/nicolas/Debian12/Fonts-ttf/ && sudo cp *.ttf /usr/share/fonts && fc-cache -f -v          # Instalar fuentes // las movemos del repo al sistema // actualizamos el cache 
 
 #======================================================================================================================================#
 echo "=== Descargando ohmyzsh - Esto puede tardar varios minutos ===="
 
-# Descarga e instalación de Oh My Zsh
-if echo "y" | sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+if echo "y" | sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then               # Descarga e instalación de Oh My Zsh
     echo "Oh My Zsh instalado con éxito"
 else
     echo "Error al instalar Oh My Zsh" >&2
@@ -24,16 +21,15 @@ fi
 
 #======================================================================================================================================#
 
-# Descargamos el plugin de sintaxis
-if git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting; then
+if git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting; then 
     echo "Plugin zsh-syntax-highlighting descargado con éxito"
 else
-    echo "Error al descargar el plugin zsh-syntax-highlighting" >&2
+    echo "Error al descargar el plugin zsh-syntax-highlighting" >&2                                                                    # Descargamos el plugin de sintaxis
     exit 1
 fi
 
-# Descargamos el plugin de sugerencias
-if git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions; then
+
+if git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions; then     # Descargamos el plugin de sugerencias
     echo "Plugin zsh-autosuggestions descargado con éxito"
 else
     echo "Error al descargar el plugin zsh-autosuggestions" >&2
@@ -42,8 +38,8 @@ fi
 
 #======================================================================================================================================#
 
-# Copiamos el archivo .zshrc personalizado
-if sudo cp -a -f /home/nicolas/debian12/Dotfiles/.zshrc /home/nicolas/; then
+
+if sudo cp -a -f /home/nicolas/debian12/Dotfiles/.zshrc /home/nicolas/; then                                                           # Copiamos el archivo .zshrc personalizado
     echo ".zshrc personalizado copiado con éxito"
 else
     echo "Error al copiar el archivo .zshrc" >&2
