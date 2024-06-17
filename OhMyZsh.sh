7428417f87c1fdba8a0cd6cda8274b843ecce660
Nicolas <nicolasperaltait@gmail.com>
2024-06-17 00:19:40 -0300

    Update OhMyZsh.sh

--- OhMyZsh.sh
+++ OhMyZsh.sh
@@ -19,6 +19,13 @@ else
     exit 1
 fi
 
+
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+
+
 #======================================================================================================================================#
 
 if git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting; then 
